dec5975f4158a6a4a14ed81e36f947f280fe2b70
nacho <nacho@DESKTOP-S4QGEUA>
2023-10-07 20:33:38 +0200

    2 commit

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,19 @@
 
 public class Calculadora {
 
+	public static int suma(int a, int b) {
+		return (a+b);
+	}
+	
+	public static int resta(int a, int b) {
+		return (a-b);
+	}
+	
+	public static int multiplicacion(int a, int b) {
+		return (a*b);
+	}
+	
+	public static int division(int a, int b) {
+		return (a/b);
+	}
 }
